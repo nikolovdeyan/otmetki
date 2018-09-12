b6d1002ad7032e0e1da65c1a9c63a2d2d50807ce
Deyan Nikolov <deyan.nikolov@gmail.com>
2018-09-12 16:54:16 +0300

    Remove most of old style. Align page with Bootstrap4

--- docs/otmetki.js
+++ docs/otmetki.js
@@ -3,39 +3,39 @@ const section = document.querySelector('section');
 const bookmarksURL = 'bookmarks.json';
 
 fetch(bookmarksURL)
-  .then(response => {
-    if(!response.ok) {
-      throw Error(response.statusText);
-    }
-    return response.json();
-  })
-  .then(bookmarksJson => {
-    populateHeader();
-    if (!bookmarksJson || !bookmarksJson.children) {
-      throw Error('Invalid bookmarksJson format.');
-    }
-    // bookmarksJson contains multiple bookmark dir contents
-    // the second one corresponds to the Bookmark Toolbar
-    const bookmarks = bookmarksJson.children[1].children;
-    populateSection(bookmarks);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+    .then(response => {
+        if(!response.ok) {
+            throw Error(response.statusText);
+        }
+        return response.json();
+    })
+    .then(bookmarksJson => {
+        populateHeader();
+        if (!bookmarksJson || !bookmarksJson.children) {
+            throw Error('Invalid bookmarksJson format.');
+        }
+        // bookmarksJson contains multiple bookmark dir contents
+        // the second one corresponds to the Bookmark Toolbar
+        const bookmarks = bookmarksJson.children[1].children;
+        populateSection(bookmarks);
+    })
+    .catch(error => {
+        console.log(error);
+    });
 
 function populateHeader() {
-  let heading = document.createElement('h1');
-  heading.textContent = 'Bookmarks';
-  header.appendChild(heading);
+    let heading = document.createElement('h2');
+    heading.textContent = 'Bookmarks';
+    header.appendChild(heading);
 }
 
 function populateSection(bookmarks) {
-  let bmElem = document.createElement('h3');
-  bookmarks.forEach(bookmark => {
-    let bmItem = createBookmarkElement(bookmark);
-    bmElem.appendChild(bmItem);
-  });
-  section.appendChild(bmElem);
+    let bmElem = document.createElement('h3');
+    bookmarks.forEach(bookmark => {
+        let bmItem = createBookmarkElement(bookmark);
+        bmElem.appendChild(bmItem);
+    });
+    section.appendChild(bmElem);
 }
 
 function createBookmarkElement(bookmark) {
@@ -54,23 +54,23 @@ function createBookmarkElement(bookmark) {
             bookmarkIcon.src = bookmark.iconuri;
             elem.appendChild(bookmarkIcon);
         }
-    if (bookmark.title) {
-      let bookmarkTitle = document.createTextNode(bookmark.title);
-      elem.appendChild(bookmarkTitle);
+        if (bookmark.title) {
+            let bookmarkTitle = document.createTextNode(bookmark.title);
+            elem.appendChild(bookmarkTitle);
+        }
+        let bookmarkLink = document.createElement('a');
+        bookmarkLink.className += ' link-text';
+        let bookmarkLinkText = document.createTextNode('   link');
+        bookmarkLink.textContent = 'link ';
+        elem.appendChild(bookmarkLink);
+        bookmarkLink.href = bookmark.uri;
+        if (bookmark.annos) {
+            let annotationElem = document.createElement('div');
+            let bookmarkAnnotation = bookmark.annos[0].value;
+            annotationElem.textContent = bookmarkAnnotation;
+            elem.appendChild(annotationElem);
+        }
     }
-    let bookmarkLink = document.createElement('a');
-    bookmarkLink.className += ' link-text';
-    let bookmarkLinkText = document.createTextNode('   link');
-    bookmarkLink.textContent = 'link ';
-    elem.appendChild(bookmarkLink);
-    bookmarkLink.href = bookmark.uri;
-    if (bookmark.annos) {
-      let annotationElem = document.createElement('div');
-      let bookmarkAnnotation = bookmark.annos[0].value;
-      annotationElem.textContent = bookmarkAnnotation;
-      elem.appendChild(annotationElem);
-    }
-  }
     else if (bookmark.type && bookmark.type === 'text/x-moz-place-container') {
         elem = document.createElement('h5');
         if (bookmark.title) {
