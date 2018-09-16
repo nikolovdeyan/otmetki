a139cd5ccf8ead5fc39faae837db9c83131c9459
Deyan Nikolov <deyan.nikolov@gmail.com>
2018-09-16 16:15:05 +0300

    Add button to copy to clipboard and further Bootstrap formatting.

--- docs/otmetki.js
+++ docs/otmetki.js
@@ -1,3 +1,4 @@
+const nav = document.querySelector('nav');
 const header = document.querySelector('header');
 const section = document.querySelector('section');
 const bookmarksURL = 'bookmarks.json';
@@ -17,7 +18,7 @@ fetch(bookmarksURL)
         // bookmarksJson contains multiple bookmark dir contents
         // the second one corresponds to the Bookmark Toolbar
         const bookmarks = bookmarksJson.children[1].children;
-        populateSection(bookmarks);
+        populateBookmarks(bookmarks);
     })
     .catch(error => {
         console.log(error);
@@ -29,7 +30,7 @@ function populateHeader() {
     header.appendChild(heading);
 }
 
-function populateSection(bookmarks) {
+function populateBookmarks(bookmarks) {
     let bmElem = document.createElement('h3');
     bookmarks.forEach(bookmark => {
         let bmItem = createBookmarkElement(bookmark);
@@ -57,10 +58,11 @@ function createBookmarkElement(bookmark) {
 }
 
 function createMozContainer(bookmark) {
-    let elem = document.createElement('div');
-    elem.className += ' card-body';
     let card = document.createElement('div');
     card.className += ' card';
+
+    let elem = document.createElement('div');
+    elem.className += ' card-body';
     if (bookmark.title) {
         let card_title = document.createElement('div');
         card_title.className += ' card-title';
@@ -82,30 +84,98 @@ function createMozContainer(bookmark) {
 
 function createMozPlace(bookmark) {
     let elem = document.createElement('li');
-    elem.className += ' moz-place';
     elem.className += ' list-group-item';
+
+    /*
     if (bookmark.iconuri) {
         let bookmarkIcon = document.createElement('img');
         bookmarkIcon.className += ' moz-place-ico';
         bookmarkIcon.src = bookmark.iconuri;
         elem.appendChild(bookmarkIcon);
     }
+    */
+
+    let spanElem = document.createElement('span');
+    spanElem.className += ' h5';
     if (bookmark.title) {
         let bookmarkTitle = document.createTextNode(bookmark.title);
-        elem.appendChild(bookmarkTitle);
-        }
-    let bookmarkLink = document.createElement('a');
-    bookmarkLink.className += ' btn btn-sm btn-outline-primary mx-2';
-    bookmarkLink.setAttribute('target', '_blank');
-    let bookmarkLinkText = document.createTextNode('link');
-    bookmarkLink.textContent = 'link';
-    elem.appendChild(bookmarkLink);
-    bookmarkLink.href = bookmark.uri;
-    if (bookmark.annos) {
-        let annotationElem = document.createElement('div');
-        let bookmarkAnnotation = bookmark.annos[0].value;
-        annotationElem.textContent = bookmarkAnnotation;
-        elem.appendChild(annotationElem);
+        spanElem.appendChild(bookmarkTitle);
     }
+    elem.appendChild(spanElem);
+
+    let openLinkBtn = document.createElement('a');
+    openLinkBtn.href = bookmark.uri;
+    openLinkBtn.textContent = 'open';
+    openLinkBtn.className += ' btn btn-sm btn-outline-primary mx-2';
+    openLinkBtn.setAttribute('target', '_blank');
+    elem.appendChild(openLinkBtn);
+
+    let copyLinkBtn = document.createElement('button');
+    copyLinkBtn.textContent = 'copy';
+    copyLinkBtn.className += ' btn btn-sm btn-outline-primary';
+    copyLinkBtn.addEventListener('click', function() {
+        const adrURI = bookmark.uri;
+        copyTextToClipboard(adrURI);
+    });
+    elem.appendChild(copyLinkBtn);
+
     return elem;
+}
+
+function copyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+
+    // Function to copy the URL of a bookmark to clipboard.
+    // See https://stackoverflow.com/a/30810322
+    // *** This styling is an extra step which is likely not required. ***
+    //
+    // Why is it here? To ensure:
+    // 1. the element is able to have focus and selection.
+    // 2. if element was to flash render it has minimal visual impact.
+    // 3. less flakyness with selection and copying which **might** occur if
+    //    the textarea element is not visible.
+    //
+    // The likelihood is the element won't even render, not even a flash,
+    // so some of these are just precautions. However in IE the element
+    // is visible whilst the popup box asking the user for permission for
+    // the web page to copy to the clipboard.
+    //
+
+    // Place in top-left corner of screen regardless of scroll position.
+    textArea.style.position = 'fixed';
+    textArea.style.top = 0;
+    textArea.style.left = 0;
+
+    // Ensure it has a small width and height. Setting to 1px / 1em
+    // doesn't work as this gives a negative w/h on some browsers.
+    textArea.style.width = '2em';
+    textArea.style.height = '2em';
+
+    // We don't need padding, reducing the size if it does flash render.
+    textArea.style.padding = 0;
+
+    // Clean up any borders.
+    textArea.style.border = 'none';
+    textArea.style.outline = 'none';
+    textArea.style.boxShadow = 'none';
+
+    // Avoid flash of white box if rendered for any reason.
+    textArea.style.background = 'transparent';
+
+
+    textArea.value = text;
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    try {
+        var successful = document.execCommand('copy');
+        var msg = successful ? 'successful' : 'unsuccessful';
+        console.log('Copying text command was ' + msg);
+    } catch (err) {
+        console.log('Oops, unable to copy');
+    }
+
+    document.body.removeChild(textArea);
 }
