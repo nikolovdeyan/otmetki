0a120e00dfbb2f3b385fe3cf2f9bc21202b551dd
Deyan Nikolov <deyan.nikolov@gmail.com>
2018-09-16 08:10:34 +0300

    Improving layout with Bootstrap4. Links to open in new window. Categories in cards.

--- docs/otmetki.js
+++ docs/otmetki.js
@@ -45,49 +45,67 @@ function createBookmarkElement(bookmark) {
         elem = document.createElement('hr');
     }
     else if (bookmark.type === 'text/x-moz-place') {
-        elem = document.createElement('li');
-        elem.className += ' moz-place';
-        elem.className += ' list-group-item';
-        if (bookmark.iconuri) {
-            let bookmarkIcon = document.createElement('img');
-            bookmarkIcon.className += ' moz-place-ico';
-            bookmarkIcon.src = bookmark.iconuri;
-            elem.appendChild(bookmarkIcon);
-        }
-        if (bookmark.title) {
-            let bookmarkTitle = document.createTextNode(bookmark.title);
-            elem.appendChild(bookmarkTitle);
-        }
-        let bookmarkLink = document.createElement('a');
-        bookmarkLink.className += ' link-text';
-        let bookmarkLinkText = document.createTextNode('   link');
-        bookmarkLink.textContent = 'link ';
-        elem.appendChild(bookmarkLink);
-        bookmarkLink.href = bookmark.uri;
-        if (bookmark.annos) {
-            let annotationElem = document.createElement('div');
-            let bookmarkAnnotation = bookmark.annos[0].value;
-            annotationElem.textContent = bookmarkAnnotation;
-            elem.appendChild(annotationElem);
-        }
+        elem = createMozPlace(bookmark);
     }
     else if (bookmark.type && bookmark.type === 'text/x-moz-place-container') {
-        elem = document.createElement('h5');
-        if (bookmark.title) {
-            elem.textContent = bookmark.title;
-        }
-        if (bookmark.children) {
-            let list = document.createElement('ul');
-            list.className += bookmark.title;
-            list.className += ' list-group';
-            bookmark.children.forEach(child => {
-                list.appendChild(createBookmarkElement(child));
-                elem.appendChild(list);
-            });
-        }
+        elem = createMozContainer(bookmark);
     }
     else {
         console.log('Unprocessed element in input.');
+    }
+    return elem;
+}
+
+function createMozContainer(bookmark) {
+    let elem = document.createElement('div');
+    elem.className += ' card-body';
+    let card = document.createElement('div');
+    card.className += ' card';
+    if (bookmark.title) {
+        let card_title = document.createElement('div');
+        card_title.className += ' card-title';
+        card_title.textContent = bookmark.title;
+        card.appendChild(card_title);
+    }
+    if (bookmark.children) {
+        let list = document.createElement('ul');
+        list.className += bookmark.title;
+        list.className += ' list-group';
+        bookmark.children.forEach(child => {
+            list.appendChild(createBookmarkElement(child));
+            elem.appendChild(list);
+        });
+    }
+    card.appendChild(elem);
+    return card;
+}
+
+function createMozPlace(bookmark) {
+    let elem = document.createElement('li');
+    elem.className += ' moz-place';
+    elem.className += ' list-group-item';
+    if (bookmark.iconuri) {
+        let bookmarkIcon = document.createElement('img');
+        bookmarkIcon.className += ' moz-place-ico';
+        bookmarkIcon.src = bookmark.iconuri;
+        elem.appendChild(bookmarkIcon);
+    }
+    if (bookmark.title) {
+        let bookmarkTitle = document.createTextNode(bookmark.title);
+        elem.appendChild(bookmarkTitle);
+        }
+    let bookmarkLink = document.createElement('a');
+    bookmarkLink.className += ' btn btn-sm btn-outline-primary mx-2';
+    bookmarkLink.setAttribute('target', '_blank');
+    let bookmarkLinkText = document.createTextNode('link');
+    bookmarkLink.textContent = 'link';
+    elem.appendChild(bookmarkLink);
+    bookmarkLink.href = bookmark.uri;
+    if (bookmark.annos) {
+        let annotationElem = document.createElement('div');
+        let bookmarkAnnotation = bookmark.annos[0].value;
+        annotationElem.textContent = bookmarkAnnotation;
+        elem.appendChild(annotationElem);
     }
     return elem;
 }
